378fab46db22163999d9e1ff4b43201094fcc0c8
SaraSlaoui1 <118104955+SaraSlaoui1@users.noreply.github.com>
2023-02-04 12:54:03 +0100

    Add files via upload

--- introduction.py
+++ introduction.py
@@ -8,7 +8,6 @@ import streamlit as st
 import sys
 
 def intro_function():
-    st.write (sys.version)
     st.header('Introduction')
     st.markdown("Etant sensible à la cause écologique et très à l'écoute de l'actualité, j'ai voulu réaliser ce projet pour appliquer les connaissances acquises lors de la formation et en apprendre d'avantage sur l'agriculture, ses caractéristiques et l'impact potentiel que le climat pourrait avoir sur celle-ci. Tout ce projet a été réalisé à partir de données trouvées en libre accès sur internet, je mets à disposition un fichier word avec le lien des différentes sources de téléchargement.")
     
